f8543b157ed3d5652ea97534c9f55816470b49a0
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-01-28 16:57:44 -0300

    Update

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,8 +1,15 @@
 let num = [5, 8, 9, 2, 3]
 
-
-num.sort()
 num.push(1)
+num.sort()
+
 console.log(num)
 console.log(`O vetor te ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
