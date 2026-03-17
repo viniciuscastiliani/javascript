6662ab38655e540c8ad9cdcd7d43edcdcf136d37
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-03-17 10:04:40 -0300

    aula 03 if else

---
+++ aula20c2/aula03.js
@@ -0,0 +1,25 @@
+/*
+
+If & Else -> Controlador de Fluxo
+
+if(se)
+Else (se não)
+
+Operadores de Comparação
+> maior que
+< menor que
+= atribuiçao de valor
+== igual que (comprando somente o valor)
+=== igual que (comparando o valor e o tipo)
+>= maior ou igual que
+<= menor ou igual que
+
+*/
+
+const NotaDoAluno = 10
+
+if (NotaDoAluno === 10) {
+    console.log("Aluno nota 10")
+} else {
+    console.log("Aluno médio")
+}
---
+++ aula20c2/desafio.js
@@ -0,0 +1,23 @@
+/*
+Desafio
+
+Utilizando if else, crie um código que:
+- verifica se uma cidade é grande ou pequena
+-Cidade grande possui mais de 300 mil habitantes
+
+*/
+
+const saoPaulo = 3000000
+const tupa = 60000
+
+if (saoPaulo >= 300000) {
+    console.log("Cidade Grande")
+} else {
+    console.log("Cidade pequena")
+}
+
+if (tupa >= 300000) {
+    console.log("Cidade Grande")
+} else {
+    console.log("Cidade Pequena")
+}
