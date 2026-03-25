854eee3fc788b2c558ada8593b85479f3d0a2df2
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-03-25 16:38:47 -0300

    Create math.js

---
+++ aula24c2/math.js
@@ -0,0 +1,12 @@
+/*
+    Math - Matemática
+        - pow(2,2) / potência / 2 elevado a 2 = 4
+        - sqrt(25,2)/ raiz / 25
+        - ceil / teto - arredondar para cima
+        - floor / chão - arredondar para baixo
+        - random() / números aleatórios
+*/
+
+const calc = Math.random() * (100 - 1) + 1;
+console.log(Math.floor(calc))
+//sorteio de números aleatórios
