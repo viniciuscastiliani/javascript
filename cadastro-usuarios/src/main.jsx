aba97f377096725fc85a9832999459be724aab10
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-04-15 15:28:36 -0300

    Continuação do projeto

--- cadastro-usuarios/src/main.jsx
+++ cadastro-usuarios/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// APP - componente react
+// creatRoot -> Criar ali o que vai ser mostrado na tela
+// render - renderizar na tela
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+   <App />
 )
