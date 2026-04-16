274a0cdced89eb761fa36c45e420462d8269a6cf
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-04-16 16:47:25 -0300

    Estilização do projeto com HTM + CSS

--- cadastro-usuarios/src/App.jsx
+++ cadastro-usuarios/src/App.jsx
@@ -2,11 +2,36 @@ import './App.css'
 
 // todo componente do react é assim: 1) é uma função, 2) ele tem um return: fora do return é cod javascript, dentro do returno é cod HTML
 function App() {
-  let name = "Vinicius"
+  
 
   return (
-    <div>
-      <p>{ name }</p>
+    <div className='app'>
+      <h1>Cadastro de Usuários</h1>
+
+      <form>
+        <input 
+          placeholder='Nome' 
+          type='text'
+        />
+
+        <input 
+          placeholder='Email' 
+          type='email'
+        />
+
+        <input 
+          placeholder='Idade'
+          type='number' 
+        />
+
+        <button type='submit'>Cadastrar</button>
+
+      </form>
+
+      <div className='user-list'>
+        <p>Usuários cadastrados</p>
+      </div>
+
     </div>
   )
 }
