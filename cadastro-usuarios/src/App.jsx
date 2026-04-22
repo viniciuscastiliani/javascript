cb5f7abcce02db57032e2b13d4b22bd30af8fbd5
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-04-22 15:53:41 -0300

    Update App.jsx

--- cadastro-usuarios/src/App.jsx
+++ cadastro-usuarios/src/App.jsx
@@ -2,13 +2,20 @@ import './App.css'
 
 // todo componente do react é assim: 1) é uma função, 2) ele tem um return: fora do return é cod javascript, dentro do returno é cod HTML
 function App() {
-  
+  function handleSubmit(event){
+    event.preventDefault()
+    
+  }
 
   return (
     <div className='app'>
       <h1>Cadastro de Usuários</h1>
 
-      <form>
+      {/* onSubmit = Ação - fazer alguma coisa quando acontecer alguma coisa */}
+
+      {/* REACT - Reagir - > quando isso acontece, faça aquilo | On determina ação - onChange (mudar) - onClick (clicar) - onSubmit (enviar) */}
+
+      <form onSubmit={handleSubmit}>
         <input 
           placeholder='Nome' 
           type='text'
