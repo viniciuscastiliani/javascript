238a9671e69a4d58c957ee1ab2c7caea09559cc1
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-05-06 16:28:35 -0300

    Inicio Curso React HDC

---
+++ curso-react-hora-de-codar/meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, React!</h1>
+      <p>Meu primeiro App.</p>
+    </div>
+  );
+}
+
+export default App;
