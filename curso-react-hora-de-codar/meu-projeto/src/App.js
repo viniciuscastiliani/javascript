b63af2b15d9fa79318862ce7fc96ba1ba7d0cc13
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-05-07 16:26:07 -0300

    Update App.js

--- curso-react-hora-de-codar/meu-projeto/src/App.js
+++ curso-react-hora-de-codar/meu-projeto/src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Vinicius'
+  const newName = name.toUpperCase()
+
+  function sum (a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
+
   return (
     <div className="App">
-      <h1>Olá, React!</h1>
-      <p>Meu primeiro App.</p>
+      <h1>Alterando JSX!</h1>
+      <p>Olá, {newName}!</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
