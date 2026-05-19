28fd6475fb38c9c4510e8bf078e0528e8906d426
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-05-19 11:01:56 -0300

    Aprofundando nas props

--- curso-react-hora-de-codar/meu-projeto/src/components/Item.js
+++ curso-react-hora-de-codar/meu-projeto/src/components/Item.js
@@ -1,9 +1,19 @@
-function Item(props) {
+import PropTypes from 'prop-types'
+
+function Item({
+    marca = 'Faltou a marca',
+    ano_lancamento = 0
+}) {
     return (
-        <>
-            <li>{props.marca}</li>
-        </>
+        <li>
+            {marca} - {ano_lancamento}
+        </li>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano_lancamento: PropTypes.number,
 }
 
 export default Item
