89493b61d0012e1aab574da0208e0359221e56b5
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-06-15 11:16:36 -0300

    Estratura do Projeto Costs

--- curso-react-hora-de-codar/costs/src/App.js
+++ curso-react-hora-de-codar/costs/src/App.js
@@ -1,9 +1,40 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Routes>
+        <Route path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/company'>
+          <Company />
+        </Route>
+
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+
+        <Route path='/newproject'>
+          <NewProject />
+        </Route>
+
+      </Routes>
+      <p>Footer</p>
+    </Router>
+  )
 }
 
 export default App;
