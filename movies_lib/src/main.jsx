51f6a7c30d388765aebe28e8fd7d253ed28908ce
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-08-27 10:56:05 -0300

    PROJETO DE FILMES COM REACT E API DO TMDB (REACT ROUTER, REACT HOOKS) 2

--- movies_lib/src/main.jsx
+++ movies_lib/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Movie from './pages/Movie.jsx';
+import Search from './pages/Search.jsx';
+import Home from './pages/Home.jsx';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
