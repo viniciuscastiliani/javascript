84c314447d9cbe306ce9660dd3fca85e705facfc
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-08-28 10:29:37 -0300

    PROJETO DE FILMES COM REACT E API DO TMDB (REACT ROUTER, REACT HOOKS)

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -7,7 +8,17 @@ function App() {
 
   return (
     <div className="App">
+
+    <h1>TESTE DO APP</h1>
+
+      <nav id="navbar"></nav>
+      <h2>
+        <Link to="/">MoviesLib</Link>
+      </h2>
+      <Link to="/movie/1">Movie</Link>
+      <Link to="/search">Search</Link>
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
   )
 }
