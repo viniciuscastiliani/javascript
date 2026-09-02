5da7fdb5843ac7890acb5abf349d225e3fc15b3e
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-02 16:38:27 -0300

    PROJETO DE FILMES COM REACT E API DO TMDB (REACT ROUTER, REACT HOOKS)

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -28,7 +29,7 @@ const Home = () => {
             <h2 className="title">Melhores filmes:</h2>
             <div className="moviesContainer">
                 { topMovies.length === 0 && <p>Carregando...</p> }
-                { topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title }</p>)}
+                { topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
             </div>
         </div>
     );
