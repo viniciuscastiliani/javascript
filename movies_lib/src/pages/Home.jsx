0caba6a9e6719c5d9102b1da8e349352fc4d4cf8
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-02 10:39:32 -0300

    PROJETO DE FILMES COM REACT E API DO TMDB (REACT ROUTER, REACT HOOKS)

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -23,7 +23,15 @@ const Home = () => {
 
     }, [])
 
-    return <div>Home</div>;
+    return (
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="moviesContainer">
+                { topMovies.length === 0 && <p>Carregando...</p> }
+                { topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title }</p>)}
+            </div>
+        </div>
+    );
 };
 
 export default Home;
