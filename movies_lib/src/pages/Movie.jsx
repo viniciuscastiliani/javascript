d0e82cdd8718e63e71d3c77cfa63776ebb6024ca
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-09 17:18:13 -0300

    BR  Pular navegação Pesquisar     Criar   Imagem do avatar PROJETO DE FILMES COM REACT E API DO TMDB (REACT ROUTER, REACT HOOKS)

--- movies_lib/src/pages/Movie.jsx
+++ movies_lib/src/pages/Movie.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BsGraphUp, BsWallet2, BsHourglassSplit, BsFillFileEarmarkTextFill } from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import './Movie.css';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
-    return <div>Movie</div>;
+    const { id } = useParams()
+    const [movie, setMovie] = useState(null)
+
+    const getMovie = async(url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovie(data);
+    }
+
+    useEffect (() => {
+        const movieURL = `${moviesURL}${id}?${apiKey}`
+        getMovie(movieURL)
+    }, [])
+
+    return <div>{movie && <>{movie.title}</>}</div>;
 };
 
 export default Movie;
