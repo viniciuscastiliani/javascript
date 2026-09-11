b576405c7d0d7664163a34285ff650b07cd394e7
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-11 16:03:41 -0300

    BR  Pular navegação Pesquisar     Criar   Imagem do avatar PROJETO DE FILMES COM REACT E API DO TMDB (REACT ROUTER, REACT HOOKS) - final

--- movies_lib/src/pages/Movie.jsx
+++ movies_lib/src/pages/Movie.jsx
@@ -20,12 +20,50 @@ const Movie = () => {
         setMovie(data);
     }
 
+    const formatCurrency = (number) => {
+        return number.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+        })
+    }
+
     useEffect (() => {
         const movieURL = `${moviesURL}${id}?${apiKey}`
         getMovie(movieURL)
     }, [])
 
-    return <div>{movie && <>{movie.title}</>}</div>;
+    return <div className="moviePage">{movie && <>
+        <MovieCard movie={movie} showLink={false} />
+        <p className="tagline">{movie.tagline}</p>
+        <div className="info">
+            <h3>
+                <BsWallet2 /> Orçamento:
+            </h3>
+            <p>{formatCurrency(movie.budget)}</p>
+        </div>
+
+        <div className="info">
+            <h3>
+                <BsGraphUp /> Receita:
+            </h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+        </div>
+
+        <div className="info">
+            <h3>
+                <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+        </div>
+
+        <div className="infoDescription">
+            <h3>
+                <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview} minutos</p>
+        </div>
+
+    </>}</div>;
 };
 
 export default Movie;
