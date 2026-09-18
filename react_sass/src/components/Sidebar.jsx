0dcd05c453d935da625548198724708f16219d1e
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-18 16:23:30 -0300

    BR  Pular navegação Pesquisar     Criar   Imagem do avatar PROJETO DE REACT E SASS PARA O SEU PORTFÓLIO - INTEGRAÇÃO DE REACT COM SASS

--- react_sass/src/components/Sidebar.jsx
+++ react_sass/src/components/Sidebar.jsx
@@ -1,7 +1,14 @@
+import Avatar from '../img/vinicius-edit.png'
+import '../styles/components/sidebar.sass'
+
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+  return <aside id="sidebar">
+    <img src={Avatar} alt='ViniciusCastiliani'></img>
+    <p className="title">Desenvolvedor</p>
+    <p>redes sociais</p>
+    <p>informações de contato</p>
+    <a href="" className="btn">Download</a>
+  </aside>
 }
 
 export default Sidebar
