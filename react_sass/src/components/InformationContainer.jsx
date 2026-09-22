4b0b77ceaef1f32818b9fab826b0bb5703747d1c
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-22 17:14:46 -0300

    PROJETO DE REACT E SASS PARA O SEU PORTFÓLIO - INTEGRAÇÃO DE REACT COM SASS

--- react_sass/src/components/InformationContainer.jsx
+++ react_sass/src/components/InformationContainer.jsx
@@ -3,9 +3,32 @@ import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from 'react-icons/ai'
 import '../styles/components/informationcontainer.sass'
 
 const InformationContainer = () => {
-  return (
-    <div>InformationContainer</div>
-  )
+  return <section id="information">
+    <div class="infoCard">
+      <AiFillPhone id="phoneIcon"/>
+      <div>
+        <h3>Telefone:</h3>
+        <p>(14) 99777-0276</p>
+      </div>
+    </div>
+
+    <div class="infoCard">
+      <AiOutlineMail id="mailIcon"/>
+      <div>
+        <h3>E-mail:</h3>
+        <p>viniciuscastiliani@gmail.com</p>
+      </div>
+    </div>
+
+    <div class="infoCard">
+      <AiFillEnvironment id="pinIcon"/>
+      <div>
+        <h3>Localização:</h3>
+        <p>Tupã/SP</p>
+      </div>
+    </div>
+
+  </section>
 }
 
 export default InformationContainer
