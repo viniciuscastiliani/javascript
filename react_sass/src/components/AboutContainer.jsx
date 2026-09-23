b12af804b2e5b63d28e385ce1d5516c3f2a4555d
Vinicius Castiliani <viniciuscastiliani@gmail.com>
2026-09-23 16:35:38 -0300

    PROJETO DE REACT E SASS PARA O SEU PORTFÓLIO - INTEGRAÇÃO DE REACT COM SASS

---
+++ react_sass/src/components/AboutContainer.jsx
@@ -0,0 +1,15 @@
+const AboutContainer = () => {
+  return <section className="aboutContainer">
+    <h2>Sobre</h2>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore atque exercitationem aliquid voluptates, ratione ea aperiam, deserunt suscipit blanditiis laborum nesciunt ullam natus nisi dolorem! Amet suscipit ratione blanditiis neque!
+    </p>
+
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore atque exercitationem aliquid voluptates, ratione ea aperiam, deserunt suscipit blanditiis laborum nesciunt ullam natus nisi dolorem! Amet suscipit ratione blanditiis neque!
+    </p>
+    
+  </section>
+}
+
+export default AboutContainer
---
+++ react_sass/src/components/ProjectsContainer.jsx
@@ -0,0 +1,14 @@
+const ProjectsContainer = () => {
+  return <section className="projectsContainer">
+    <h2>Projetos</h2>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore atque exercitationem aliquid voluptates, ratione ea aperiam, deserunt suscipit blanditiis laborum nesciunt ullam natus nisi dolorem! Amet suscipit ratione blanditiis neque!
+    </p>
+
+    <a href="" className="btn">
+      Ver projetos
+    </a>
+  </section>
+}
+
+export default ProjectsContainer
